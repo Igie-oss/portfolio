93b2a6211a5ad37149b7cc9b58e9026bccc83c42
Igie <baldesansoigieboysanchez@gmail.com>
2022-12-01 14:53:18 +0800

    Adding dist

---
+++ dist/assets/ProjectList.5b78b1af.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.128594b5.js";import{s as e}from"./data.bcdf2a9d.js";const s=t=>r("div",{}),a=()=>r("section",{className:"w-screen h-screen pt-10 flex flex-wrap",children:r(s,{...e})});export{a as default};
